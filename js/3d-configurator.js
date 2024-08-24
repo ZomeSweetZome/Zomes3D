45a34eec17c990c3e14fba6816484b3561adce2b
Paul Voronin <voron.paul@gmail.com>
2024-08-25 00:53:36 +0300

    ✨ added handlers for furniture selector

--- js/3d-configurator.js
+++ js/3d-configurator.js
@@ -2761,22 +2761,68 @@ function dimensionsBtnHandler() {
 function furnitureBtnHandler() {
   $('#button_furniture').on('click', function () {
     $(this).toggleClass('active');
-    $('.canvas_buttons__radio').toggleClass('hidden');
-
+  
     if ($(this).hasClass('active')) {
       $('#button_dimensions').addClass('disabled');
       !isCameraInside && $('#button_camera_inside').click();
-      furnitureController();
+      furnitureController(true);
     } else {
       if (!$('#button_annotation').hasClass('active')) {
         $('#button_dimensions').removeClass('disabled');
       }
+      furnitureController(false);
     }
   });
 }
 
-function furnitureController() {
-  //! TODO
+function furnitureController(value) {
+  if (value) {
+    $('.canvas_buttons__radio').removeClass('hidden');
+    //! TODO furniture selector handler
+
+    $('#button_sleep').on('click', function () {
+      $(this).toggleClass('active');
+      $('#button_work').removeClass('active');
+      $('#button_live').removeClass('active');
+    });
+
+    $('#button_work').on('click', function () {
+      $(this).toggleClass('active');
+      $('#button_sleep').removeClass('active');
+      $('#button_live').removeClass('active');
+    });
+
+    $('#button_live').on('click', function () {
+      $(this).toggleClass('active');
+      $('#button_sleep').removeClass('active');
+      $('#button_work').removeClass('active');
+    });
+
+    if ($('#button_sleep').hasClass('active')) {
+      //! turn OFF furniture: work
+      //! turn OFF furniture: live
+      
+      //! turn ON furniture: sleep
+    }
+
+    if ($('#button_work').hasClass('active')) {
+      //! turn OFF furniture: sleep
+      //! turn OFF furniture: live
+      
+      //! turn ON furniture: work
+    }
+
+    if ($('#button_live').hasClass('active')) {
+      //! turn OFF furniture: sleep
+      //! turn OFF furniture: work
+
+      //! turn ON furniture: live
+    }
+
+  } else {
+    $('.canvas_buttons__radio').addClass('hidden');
+    //! TODO turn off all furniture
+  }
 }
 
 
